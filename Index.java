40d38b8aaddf7c0aee7c8f260ce414396e51291b
AbdulrahmanAlzomea <444101078@student.ksu.edu.sa>
2024-11-23 17:27:11 +0300

    i added the index class

---
+++ Index.java
@@ -0,0 +1,62 @@
+
+public class Index {
+    LinkedList<Document> documents;
+
+    public Index() {
+        documents = new LinkedList<Document>();
+    }
+
+    public void addDocument(Document document) {
+        documents.insert(document);
+    }
+
+    public Document getDocument(int id) {
+        if (documents.empty()) {
+            System.out.println("No documents found");
+            return null;
+        }
+        documents.findFirst();
+        while (documents.retrieve() != null) {
+            if (documents.retrieve().id == id) {
+                return documents.retrieve();
+            }
+            if (documents.last()) {
+                break;
+
+            }
+            documents.findNext();
+        }
+
+        return null;
+    }
+
+
+    public void displayDoc() {
+        if (documents.empty()) {
+            System.out.println("No documents found");
+            return;
+        }
+
+        documents.findFirst();
+        while (true) { // Use a loop to iterate over all documents
+            Document document = documents.retrieve();
+            System.out.print("Doc ID (" + document.id + ") "); // Print the document ID
+            System.out.print("["); // Start the word list
+
+            // Iterate over the words in the document
+            document.words.findFirst();
+            while (true) {
+                System.out.print("\"" + document.words.retrieve() + "\""); // Print each word in quotes
+                if (document.words.last()) break; // If it's the last word, stop
+                System.out.print(", "); // Add a comma and space between words
+                document.words.findNext();
+            }
+            System.out.println("]"); // Close the word list
+
+            if (documents.last()) break; // If it's the last document, stop
+            documents.findNext(); // Move to the next document
+        }
+    }
+
+
+}
