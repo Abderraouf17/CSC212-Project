0d1a3a7e3b8459ec2376f445f8d44d96381edc2e
AbdulrahmanAlzomea <444101078@student.ksu.edu.sa>
2024-11-23 21:29:05 +0300

    i added the BST Node

---
+++ BSTNode.java
@@ -0,0 +1,11 @@
+public class BSTNode<T> {
+    int key;
+    T data;
+    BSTNode<T> left, right;
+
+    public BSTNode(int key, T data) {
+        this.key = key;
+        this.data = data;
+        left = right = null;
+    }
+}
